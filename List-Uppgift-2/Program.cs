e27c122d537f9f111a82f4652aa533905172a0d2
Admin <Admin@TEB123018>
2024-01-31 11:02:29 +0100

    Add project files.

---
+++ List-Uppgift-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nummerLista = new();
+            string input = " ";
+
+            Console.WriteLine("Skriv in ett antal nummer på nya rader, skriv en tom rad när du är nöjd.");
+
+            while (input != "")
+            {
+                    input = Console.ReadLine();
+                switch (input)
+                {
+                    case "":
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.Write("Skriv in nummer:");
+                        nummerLista.Add(int.Parse(input));
+                        break;
+                }
+            }
+            Max(nummerLista);
+        }
+
+        static void Max(List<int> nummerLista)
+        {
+            nummerLista.Sort();
+            Console.WriteLine(nummerLista[nummerLista.Count()-1]);
+        }
+    }
+}
